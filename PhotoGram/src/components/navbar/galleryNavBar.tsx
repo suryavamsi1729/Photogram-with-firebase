915c86b8a301c6c8b9c9038f58aa7ef62b124a4e
suryavamsi1729 <suryavamsi2005doddi@gmail.com>
2025-01-23 20:01:05 +0530

    changes

--- PhotoGram/src/components/navbar/galleryNavBar.tsx
+++ PhotoGram/src/components/navbar/galleryNavBar.tsx
@@ -27,9 +27,6 @@ const GalleryNavBar : React.FC<IGalleryNavBar> = ()=>{
                             return (
                                 <NavBarItem onClick={()=>{navigate(item.link)}} key={i} className={"w-auto h-auto flex flex-col justify-center items-center hover:cursor-pointer"}>
                                     <div className={`group relative w-auto h-full px-4 flex flex-col justify-between items-center`}>
-                                        {/* <svg className={`w-9 h-9  group-hover:fill-white ${navgationCheck(location.pathname,item.link)?"fill-white":"fill-white/40"}`} xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" >
-                                            <path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h560q33 0 56.5 23.5T840-760v560q0 33-23.5 56.5T760-120H200Zm0-80h133v-133H200v133Zm213 0h134v-133H413v133Zm214 0h133v-133H627v133ZM200-413h133v-134H200v134Zm213 0h134v-134H413v134Zm214 0h133v-134H627v134ZM200-627h133v-133H200v133Zm213 0h134v-133H413v133Zm214 0h133v-133H627v133Z"/>
-                                        </svg> */}
                                         <Grid3X3Icon className={`w-9 h-9  group-hover:fill-white ${navgationCheck(location.pathname,item.link)?"fill-white":"fill-white/40"}`}/>
                                         <div className="absolute -bottom-2 w-full h-[2px] rounded-lg bg-white"></div>
                                     </div>
